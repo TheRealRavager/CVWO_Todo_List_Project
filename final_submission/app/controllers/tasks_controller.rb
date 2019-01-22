c362513d02c01103312f4fbc884668511963d51c
TheRealRavager <iskandar@u.nus.edu>
2019-01-23 02:21:34 +0800

    Fully implemented create (persistent list) and added update functionality (editing of current tasks). Added personal_notes folder and created notes.md to document learning (going to cut out unnecessary comments in app itself and shift them into notes.md)

--- final_submission/app/controllers/tasks_controller.rb
+++ final_submission/app/controllers/tasks_controller.rb
@@ -1,15 +1,32 @@
 class TasksController < ApplicationController
 	def index
-		def index
-			@tasks = Task.all #returns an active record relation
-		end
-  end
+		@tasks = Task.all #returns an active record relation
+	end
 
 	def new
 		@task = Task.new
 	end
 	
+	#is called when submit is pressed on form. When submit is pressed, it outputs a hash of parameters included in the form.
 	def create
+		@task = Task.new(task_params)
+		@task.save
+		redirect_to tasks_path #redirects user to main task page (tasks#index)
 		#TODO send task to database then throw the user back to the all tasks page.
+	end
+
+	def edit
+		@task = Task.find(params[:id]) #find the specific task with that id
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update(task_params)
+		redirect_to tasks_path
+	end
+
+	private #prevents params other than those listed in permit from being passed (security issue)
+	def task_params
+		params.require(:task).permit(:title, :details)
 	end
 end
---
+++ final_submission/personal_notes/Notes.md
@@ -0,0 +1,9 @@
+Common Errors:
+
+1. 'Missing a template' -> No view for that method
+
+2. 'Argument cant be nil' -> not specifying which specific object to look for
+
+Misc Notes:
+
+1. Views can be shared in 'partials'
