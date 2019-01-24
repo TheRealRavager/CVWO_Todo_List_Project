61d36f8a5eb73b8e663355ed4a888671b130c548
TheRealRavager <iskandar@u.nus.edu>
2019-01-24 11:44:19 +0800

    reset database and added testing tasks in seeds.rb

--- final_submission/db/seeds.rb
+++ final_submission/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create([
+	{title: 'First Task',
+	details: 'This is the first task'},
+	{title: 'Second Task',
+	details: 'This is the second task'}
+])
