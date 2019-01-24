534d185817a3a078ae62d2d32981b9d60214898b
TheRealRavager <iskandar@u.nus.edu>
2019-01-24 22:40:30 +0800

    generated sessions controller and login view

--- final_submission/config/routes.rb
+++ final_submission/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  get 'tasks/index'
-  get 'tasks/new'
   get 'welcome/index'
 	root 'welcome#index' #root sets the 'main' webpage as /welcome/index. index is a method thats why its welcome#index
+	get 'tasks/index'
+  get 'tasks/new'
+	get 'log_in', to: 'sessions#new'
+	get 'log_out', to: 'sessions#destroy'
+	resources :users
+	get 'sign_up', to: 'users#new'
 	resources :tasks #this creates routes between app and the tasks model
+	resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
