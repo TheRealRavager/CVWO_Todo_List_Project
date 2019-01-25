d4dca3bd44a10e78108ffd24b282b0763b998a2f
TheRealRavager <iskandar@u.nus.edu>
2019-01-25 23:00:12 +0800

    replaced sqlite3 with postgresql for Heroku deployment

--- final_submission/db/schema.rb
+++ final_submission/db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2019_01_24_074407) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -29,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_01_24_074407) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
