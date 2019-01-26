eba2bdfdbae8c1db6f3bd5fda97101f86b49405c
TheRealRavager <iskandar@u.nus.edu>
2019-01-27 00:35:02 +0800

    added users to History

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_144628) do
+ActiveRecord::Schema.define(version: 2019_01_26_163356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "histories", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
+    t.date "deadline"
+    t.date "completion_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -35,5 +47,6 @@ ActiveRecord::Schema.define(version: 2019_01_26_144628) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "histories", "users"
   add_foreign_key "tasks", "users", on_delete: :cascade
 end
