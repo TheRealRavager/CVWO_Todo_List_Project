1147fe72c865387f84c5239bcfe85c8eb466570a
TheRealRavager <iskandar@u.nus.edu>
2019-01-26 20:16:01 +0800

    added completion_date to database. Made tasks dependent on their user and destroyed when the user is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	has_secure_password
-	has_many :tasks
+	has_many :tasks, dependent: :destroy
 	validates :username, uniqueness: {case_sensitive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_075956) do
+ActiveRecord::Schema.define(version: 2019_01_26_120851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_075956) do
     t.boolean "completed", default: false
     t.bigint "user_id"
     t.date "deadline"
+    t.date "completion_date"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -33,5 +34,5 @@ ActiveRecord::Schema.define(version: 2019_01_26_075956) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "tasks", "users"
+  add_foreign_key "tasks", "users", on_delete: :cascade
 end
