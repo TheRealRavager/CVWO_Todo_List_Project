1b31dc9be1f2f61de58f013951dd2e51cd785721
TheRealRavager <iskandar@u.nus.edu>
2019-01-27 04:16:33 +0800

    added role attribute to all users. only admin can view/edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_191719) do
+ActiveRecord::Schema.define(version: 2019_01_26_200935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_191719) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
   add_foreign_key "histories", "users"
