b6f9bbb639f94336af763c296b9039bfd0824ce7
TheRealRavager <iskandar@u.nus.edu>
2019-01-27 11:39:52 +0800

    made admins undestroyable, upon cancel of user create/update, admins are now sent to users page (instead of root path). added more HTML formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,64 +2,71 @@ class UsersController < ApplicationController
 	before_action :boot_non_admins, except: [:new, :create, :edit, :update] #need to add a way for users to edit only their own details
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
+
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+
 	def edit
 		check_user unless check_admin
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+	def create
+		# If user creation or edit is cancelled, send admins back to users page and non-admins to main page
+		if params[:cancel]
+			check_admin ? redirect_to users_path : redirect_to root_path
+		else
+			@user = User.new(user_params)
+			respond_to do |format|
+				if @user.save
+					format.html { redirect_to root_path, notice: 'User was successfully created.' }
+				else
+					format.html { render :new }
+					format.json { render json: @user.errors, status: :unprocessable_entity }
+				end
+			end
+		end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
 	def update
-		respond_to do |format|
-			if @user.update(user_params)
-				format.html { redirect_to root_path, notice: 'User was successfully updated.' }
-				format.json { render :show, status: :ok, location: @user }
-			else
-				format.html { render :edit }
-				format.json { render json: @user.errors, status: :unprocessable_entity }
+		if params[:cancel]
+			redirect_to root_path
+		else
+			respond_to do |format|
+				if @user.update(user_params)
+					format.html { redirect_to root_path, notice: 'User was successfully updated.' }
+					format.json { render :show, status: :ok, location: @user }
+				else
+					format.html { render :edit }
+					format.json { render json: @user.errors, status: :unprocessable_entity }
+				end
 			end
 		end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+	def destroy
+		if !check_admin
+    	@user.destroy
+			respond_to do |format|
+				format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+				format.json { head :no_content }
+			end
+		else
+			flash.alert = 'Error! Unable to destroy administrator accounts!'
+			render 'index'
+		end
   end
 
   private
@@ -70,7 +77,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation, :role)
+      params.require(:user).permit(:username, :password, :password_confirmation, :role, :cancel)
 		end
 		
 
